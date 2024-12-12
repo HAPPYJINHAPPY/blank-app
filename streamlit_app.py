bf633d96d04b99ba840dda74af4598e7815e7812
HAPPYJINHAPPY <1466608106@qq.com>
2024-12-12 11:44:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,18 +14,15 @@ import random
 import plotly.express as px
 import matplotlib
 from matplotlib import rcParams
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-local_css("style.css")
+!apt-get update -y
+!apt-get install -y fonts-noto-cjk
+matplotlib.rcParams['font.sans-serif'] = ['Noto Sans CJK SC']  # 使用 Noto Sans CJK SC
+matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 # 设置中文字体为 SimHei（黑体）
 rcParams['font.sans-serif'] = ['SimHei']  # 或者 ['Microsoft YaHei']，根据系统支持选择
 rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False#用来正常显示负号
-# 配置 Matplotlib 使用中文字体
-matplotlib.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 使用微软雅黑字体
-matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 # Load the uploaded file
 file_path = 'corrected_fatigue_simulation_data.csv'
 data = pd.read_csv(file_path)
